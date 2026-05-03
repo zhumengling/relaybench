2fb2f01fb97a0cdfbe62a88ca520a05de0c92355
zhumengling <10433941@qq.com>
2026-05-03 21:25:46 +0800

    Auto-collapse transparent proxy drawer on outside click

--- RelayBench.App/Views/Pages/TransparentProxyPage.xaml.cs
+++ RelayBench.App/Views/Pages/TransparentProxyPage.xaml.cs
@@ -1,5 +1,9 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using RelayBench.App.ViewModels;
 
 namespace RelayBench.App.Views.Pages;
 
@@ -17,4 +21,38 @@ public partial class TransparentProxyPage : UserControl
             mainWindow.ToggleFloatingTokenMeterFromUi();
         }
     }
+
+    private void TransparentProxyPageRoot_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (DataContext is not MainWindowViewModel viewModel ||
+            !viewModel.IsTransparentProxySettingsDrawerOpen ||
+            e.OriginalSource is not DependencyObject source ||
+            IsDescendantOf(source, TransparentProxySettingsDrawerHost))
+        {
+            return;
+        }
+
+        if (viewModel.ToggleTransparentProxySettingsDrawerCommand.CanExecute(null))
+        {
+            viewModel.ToggleTransparentProxySettingsDrawerCommand.Execute(null);
+        }
+    }
+
+    private static bool IsDescendantOf(DependencyObject source, DependencyObject ancestor)
+    {
+        for (var current = source; current is not null; current = GetParent(current))
+        {
+            if (ReferenceEquals(current, ancestor))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static DependencyObject? GetParent(DependencyObject source)
+        => source is Visual or Visual3D
+            ? VisualTreeHelper.GetParent(source)
+            : LogicalTreeHelper.GetParent(source);
 }
